37a8d8d8f46711ba5479e876a6ec4d2c8f1726c7
Tomas Masak <03thomasmasak@gmail.com>
2024-07-28 10:32:21 +0900

    removed tags and ttl

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
+resource "aws_dynamodb_table" "user_profiles" {
   name           = "UserProfiles"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
@@ -6,15 +6,5 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   attribute {
     name = "id"
     type = "S"
-  }
-
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = true
-  }
-
-  tags = {
-    Name        = "dynamodb-table-1"
-    Environment = "feature/user-table"
   }
 }
