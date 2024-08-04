44709636197be06c6107ba7a6e42e96dd5acf5f2
Tomas Masak <102941629+ThomasMasak@users.noreply.github.com>
2024-08-04 17:00:57 +0900

    Feature/lambda (#6)
    * updated lambda.tf with a starter image
    
    * modified .py script

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,8 +1,22 @@
+data "aws_ecr_authorization_token" "token" {}
+
 resource "aws_ecr_repository" "user_service" {
   name                 = "user_service"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = false
+  }
+  
+  lifecycle {
+      ignore_changes = all
+    }
+
+  provisioner "local-exec" {
+    command = <<-EOF
+          docker build ../ -t ${aws_ecr_repository.user_service.repository_url}:latest; \
+          docker login -u AWS -p ${data.aws_ecr_authorization_token.token.password} ${data.aws_ecr_authorization_token.token.proxy_endpoint}; \
+          docker push ${aws_ecr_repository.user_service.repository_url}:latest
+        EOF
   }
 }
