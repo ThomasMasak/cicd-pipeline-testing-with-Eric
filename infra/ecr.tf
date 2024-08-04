6377c89dc6e69c75bccb289c80d18651f40e61e3
Tomas Masak <03thomasmasak@gmail.com>
2024-08-04 10:53:13 +0900

    updated workflow, python script and tf files for lambda deployment from ecr

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -6,3 +6,16 @@ resource "aws_ecr_repository" "user_service" {
     scan_on_push = false
   }
 }
+
+resource "null_resource" "ecr_image" {
+  triggers = {
+    python_file = md5(file("../src/user_service.py"))
+    docker_file = md5(file("../Dockerfile"))
+  }
+}
+
+data "aws_ecr_image" "lambda_image" {
+  depends_on = [null_resource.ecr_image]
+  repository_name = aws_ecr_repository.user_service
+  image_tag       = "latest"
+}
---
+++ infra/lambda.tf
@@ -0,0 +1,72 @@
+data "aws_ecr_repository" "user_service" {
+  name = "user_service"
+}
+
+resource "aws_lambda_function" "user_service" {
+  depends_on = [
+    null_resource.image_ecr,
+  ]
+
+  function_name = "user_service"
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.user_service.repository_url}:latest"
+  role          = aws_iam_role.lambda_user_service.arn
+  publish       = true
+
+  memory_size = 128
+  timeout     = 28
+
+  # lifecycle {
+  #   ignore_changes = [
+  #     image_uri, last_modified
+  #   ]
+  # }
+}
+
+resource "aws_cloudwatch_log_group" "lambda_user_service" {
+  name              = "/aws/lambda/lambda_user_service"
+  retention_in_days = 7
+}
+
+resource "aws_iam_role" "lambda_user_service" {
+  name               = "lambda-user-service-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_user_service_assume.json
+}
+
+data "aws_iam_policy_document" "lambda_user_service_assume" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "lambda.amazonaws.com",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "user_service" {
+  role       = aws_iam_role.lambda_user_service.name
+  policy_arn = aws_iam_policy.lambda_user_service_custom.arn
+}
+
+resource "aws_iam_policy" "lambda_user_service_custom" {
+  name   = "lambda-user-service-policy"
+  policy = data.aws_iam_policy_document.lambda_user_service_custom.json
+}
+
+data "aws_iam_policy_document" "lambda_user_service_custom" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+    sid       = "CreateCloudWatchLogs"
+  }
+}
