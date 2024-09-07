1a591ca290a1355eefb43f5f3ba44df27250e500
Tomas Masak <102941629+ThomasMasak@users.noreply.github.com>
2024-09-08 08:58:42 +0900

    Feature/app sync (#8)
    * Sepparated IAM roles and policies to its own .tf file, added dynamodb full access policy to lambda
    
    * AppSync GraphQL wiring set up

---
+++ infra/appsync.tf
@@ -0,0 +1,86 @@
+resource "aws_appsync_graphql_api" "main" {
+  authentication_type = "AWS_IAM"
+  name                = "terraforming-appsync"
+  schema              = file("${path.module}/schema.graphql")
+  
+  log_config {
+    cloudwatch_logs_role_arn = aws_iam_role.graph_log_role.arn
+    field_log_level          = "ALL"
+    exclude_verbose_content  = false
+  }
+
+  additional_authentication_provider {
+    authentication_type = "API_KEY"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "graph" {
+  name = "graph"
+
+  tags = {
+    application = "graph"
+  }
+}
+
+### AppSync Wiring to Lambda ###
+
+resource "aws_appsync_datasource" "user_service" {
+  api_id           = aws_appsync_graphql_api.main.id
+  name             = "user_service"
+  service_role_arn = aws_iam_role.appsync_user_service.arn
+  type             = "AWS_LAMBDA"
+
+  lambda_config {
+    function_arn = aws_lambda_function.user_service.arn
+  }
+}
+
+resource "aws_appsync_function" "user_service_lambda" {
+  api_id                   = aws_appsync_graphql_api.main.id
+  data_source              = "user_service"
+  name                     = "user_service_lambda"
+  request_mapping_template = <<EOF
+    {
+        "version": "2017-02-28",
+        "operation": "Invoke",
+        "payload": {
+            "command" : "$ctx.info.fieldName",
+            "payload" : $utils.toJson({$ctx.args})
+        }
+    }
+  EOF
+
+  response_mapping_template = <<EOF
+    $utils.toJson($context.result)
+  EOF
+}
+
+
+resource "aws_appsync_resolver" "user_service_lambda_query" {
+  type              = "Query"
+  api_id            = aws_appsync_graphql_api.main.id
+  field             = "getUser"
+  request_template  = "{}"
+  response_template = "$util.toJson($ctx.prev.result)"
+  kind              = "PIPELINE"
+  pipeline_config {
+    functions = [
+      aws_appsync_function.user_service_lambda.function_id
+    ]
+  }
+}
+
+
+resource "aws_appsync_resolver" "user_service_lambda_mutation" {
+  type              = "Mutation"
+  api_id            = aws_appsync_graphql_api.main.id
+  field             = "createUser"
+  request_template  = "{}"
+  response_template = "$util.toJson($ctx.prev.result)"
+  kind              = "PIPELINE"
+  pipeline_config {
+    functions = [
+      aws_appsync_function.user_service_lambda.function_id
+    ]
+  }
+}
--- infra/iam.tf
+++ infra/iam.tf
@@ -1,4 +1,7 @@
-resource "aws_iam_role" "iam_lambda_user_service" {
+### Lambda ###
+
+
+resource "aws_iam_role" "lambda_user_service" {
   name               = "lambda-user-service-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_user_service_assume.json
 }
@@ -19,7 +22,7 @@ data "aws_iam_policy_document" "lambda_user_service_assume" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_add" {
-  role       = aws_iam_role.iam_lambda_user_service.name
+  role       = aws_iam_role.lambda_user_service.name
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
@@ -41,16 +44,101 @@ data "aws_iam_policy_document" "lambda_policy" {
   }
 }
 
+
+### Lambda & DynamoDB ###
+
+
 resource "aws_iam_role_policy" "dynamodb-lambda-policy" {
   name   = "dynamodb-lambda-policy"
-  role   = aws_iam_role.iam_lambda_user_service.id
+  role   = aws_iam_role.lambda_user_service.id
   policy = data.aws_iam_policy_document.dynamodb_lambda_policy.json
-  }
+}
 
 data "aws_iam_policy_document" "dynamodb_lambda_policy" {
   statement {
-    actions = ["dynamodb:*"]
+    actions   = ["dynamodb:*"]
     effect    = "Allow"
     resources = [aws_dynamodb_table.user_profiles.arn]
   }
+}
+
+
+### AppSync Wiring to Lambda ###
+
+
+resource "aws_iam_role" "appsync_user_service" {
+  name = "appsync-user-service-role"
+
+  assume_role_policy = data.aws_iam_policy_document.appsync_user_service.json
+}
+
+data "aws_iam_policy_document" "appsync_user_service" {
+  statement {
+    sid = "1"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["appsync.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "appsync_user_service_doc" {
+  statement {
+    sid = "1"
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+    resources = [aws_lambda_function.user_service.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "appsync_user_service" {
+  name   = "appsync_user_service_role_policy"
+  role   = aws_iam_role.appsync_user_service.id
+  policy = data.aws_iam_policy_document.appsync_user_service_doc.json
+}
+
+
+### AppSync CloudWatch ###
+
+
+data "aws_iam_policy_document" "graph_log_role" {
+  statement {
+    sid = "1"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["appsync.amazonaws.com"]
+    }
+  }
+
+}
+
+resource "aws_iam_role" "graph_log_role" {
+  name = "graph_log_role"
+
+  assume_role_policy = data.aws_iam_policy_document.graph_log_role.json
+}
+
+data "aws_iam_policy_document" "graph_log_policy" {
+  statement {
+    sid = "1"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "graph_log" {
+  name   = "graph_log_policy"
+  role   = aws_iam_role.graph_log_role.id
+  policy = data.aws_iam_policy_document.graph_log_policy.json
 }
