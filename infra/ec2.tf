2d3c23873ece1d4bfab470a2ca890aa038811192
Tomas Masak <03thomasmasak@gmail.com>
2024-09-19 21:31:03 +0900

    Removed unnecessary ssh keys

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web3" {
+resource "aws_instance" "web" {
   ami           = "ami-0a70c5266db4a6202"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
@@ -10,17 +10,8 @@ resource "aws_instance" "web3" {
   }
 }
 
-resource "tls_private_key" "default" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = tls_private_key.default.public_key_openssh
 }
 
-resource "local_file" "tf_key" {
-  content  = tls_private_key.default.private_key_pem
-  filename = ".id_rsa"
-}
